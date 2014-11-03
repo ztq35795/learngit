b62fb8e9c27f8efa4c5b92e0c2599164ad83910e
ztq35795 <1014472410@qq.com>
2014-11-03 21:20:33 +0000

    初步完成ls功能

--- py1.py
+++ py1.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
 'a test module'
 __author__ = 'Billion Zhan'
-print ("hello world")
+import os
+filename=os.listdir(".")
+for name in filename:
+    print("%s "%(name),end=' ')
+print()
